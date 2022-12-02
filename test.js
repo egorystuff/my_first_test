14cec4f816d962115ff7fc9e1017bd6919d1021b
egorystuff <mr.yankovec@gmail.com>
2022-12-02 21:42:00 +0300

    test.js
    added file test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+var myCrazyObject = {
+  name: 'Нелепый объект',
+  'some array': [7, 9, { purpose: 'путаница', number: 123 }, 3.3],
+  'random animal': 'Банановая акула',
+};
+
+// console.log(myCrazyObject['some array'][2].number);
+console.log(myCrazyObject);
+console.warn('ahahha');
+console.warn('olegovren');
